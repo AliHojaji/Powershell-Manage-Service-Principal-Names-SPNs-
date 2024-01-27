dadba4e37b7a498ffba13f7d370812230e31db43
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 12:08:17 +0330

    Add files via upload

---
+++ Manage Service Principal Names (SPNs).ps1
@@ -0,0 +1,20 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*----------------------------------*--#
+#---> Managed Service Accounts (MSA) <---#
+#--*----------------------------------*--#
+
+#--> Create a managed service account
+New-ADServiceAccount -Name MSA-Testsrv -RestrictToSingleComputer
+Add-ADComputerServiceAccount -Identity DC-Test1 -ServiceAccount MSA-Testsrv
+Test-ADServiceAccount -Identity MSA-Testsrv
+
+#--> Install MSA on local DC-TEST1
+Install-ADServiceAccount MSA-Testsrv
+Test-ADServiceAccount -Identity MSA-Testsrv
+
+#--> Create a dummy service
+New-Service -Name TESTSRV -BinaryPathName C:\Windows\System32\notepad.exe
+
+
+##--> Group Managed Service Accounts (GMSA) <--#
